18a12e57a776c4904a26b68387df5c3d81465ee7
zahra kheiri <zahrakheiri.eng@gmail.com>
2024-05-31 17:01:25 +0330

    work experinces page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,12 @@ import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { SkillsComponent } from './skills/skills.component';
+import { WorkExperincesComponent } from './work-experinces/work-experinces.component';
 
 const routes: Routes = [
   {path:'',redirectTo:'Home',pathMatch:'full'},
   {path:'Home',component:HomeComponent,title:'Home',pathMatch:'full'},
+  {path:'WorkExperiences',component:WorkExperincesComponent,title:'Home',pathMatch:'full'},
   {path:'Skills',component:SkillsComponent,title:'Skills',pathMatch:'full'},
   {path:'Contact',component:ContactComponent,title:'Contact',pathMatch:'full'},
   {path:'About',component:AboutComponent,title:'About'},
